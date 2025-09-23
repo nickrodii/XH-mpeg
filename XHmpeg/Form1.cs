e1614a3afb93a290a76d62dcddeb61e4421dfb3f
nickrodii <nickjrodi@gmail.com>
2025-09-23 00:01:39 -0700

    attempt at uploading files again

--- XHmpeg/Form1.cs
+++ XHmpeg/Form1.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -7,6 +10,9 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
+        // storing paths of ffmpeg and ffprobe executables
+        string ffmpegPath = Path.Combine(AppContext.BaseDirectory, "tools", "ffmpeg", "ffmpeg.exe");
+        string ffprobePath = Path.Combine(AppContext.BaseDirectory, "tools", "ffmpeg", "ffprobe.exe");
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -27,12 +33,35 @@ namespace WinFormsApp1
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
         {
             if (videoFileDialog.ShowDialog() == DialogResult.OK)
             {
                 videoDirectory.Text = videoFileDialog.FileName;
+                string args = $"-v quiet -print_format json -show_format -show_streams \"{videoFileDialog.FileName}\"";
+                string json = await RunProcessAsync(ffprobePath, args);
+
+                MessageBox.Show(json.Substring(0, Math.Min(500, json.Length)),
+                    "ffprobe output (first 500 chars)");
             }
+        }
+
+        private static async Task<string> RunProcessAsync(string exe, string args)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = exe,
+                Arguments = args,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using var p = Process.Start(psi)!;
+            string output = await p.StandardOutput.ReadToEndAsync();
+            await p.WaitForExitAsync();
+            return output;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -107,6 +136,16 @@ namespace WinFormsApp1
         }
 
         private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void videoSaveDestination_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label22_Click(object sender, EventArgs e)
         {
 
         }
